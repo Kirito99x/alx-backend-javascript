216ca145ed5cca79c2eff6593b9dabced3432761
Kirito99x <msaifeldin46@gmail.com>
2024-06-27 15:12:55 +0300

    fixes

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -6,12 +6,4 @@ export default function createIteratorObject(report) {
       }
     }
   }());
-}export default function createIteratorObject(report) {
-  return (function* _() {
-    for (const department of Object.values(report.allEmployees)) {
-      for (const employee of department) {
-        yield employee;
-      }
-    }
-  }());
 }
