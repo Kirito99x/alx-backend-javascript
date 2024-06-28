4a1a69a46c908e05c3edb536679e5fe0e6c8d583
Kirito99x <msaifeldin46@gmail.com>
2024-06-28 16:05:34 +0300

    first_task

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,1 +1,9 @@
-#!/usr/bin/node
+import ClassRoom from './0-classroom';
+
+/**
+ * Creates array of {@link ClassRoom}s with specific size.
+ * @returns array of {@link ClassRoom}s.
+ */
+export default function initializeRooms() {
+  return [19, 20, 34].map((size) => new ClassRoom(size));
+}
