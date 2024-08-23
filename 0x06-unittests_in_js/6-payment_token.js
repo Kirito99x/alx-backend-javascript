b1c2a154f94dc9744413804f24ccee39f0da7375
Kirito99x <msaifeldin46@gmail.com>
2024-08-23 03:26:32 +0200

    fx

--- 0x06-unittests_in_js/6-payment_token.js
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -1,1 +1,8 @@
-#!/usr/bin/node
+const getPaymentTokenFromAPI = (success) => new Promise((resolve, _reject) => {
+    if (success) {
+      resolve({data: 'Successful response from the API'});
+    }
+  });
+  
+  module.exports = getPaymentTokenFromAPI;
+  
